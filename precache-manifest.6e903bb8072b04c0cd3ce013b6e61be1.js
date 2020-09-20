3ded14f513696cb94df6805a35616276ad1a933d
Michael Hoha <michael1hoha@gmail.com>
2020-09-20 23:26:39 +0300

    Updates

---
+++ precache-manifest.6e903bb8072b04c0cd3ce013b6e61be1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8fdaa2de6e9796a7450e227ed98b32",
+    "url": "/MovieApp/index.html"
+  },
+  {
+    "revision": "ed0e4bf1bb43dacd7277",
+    "url": "/MovieApp/static/css/main.3958fb7c.chunk.css"
+  },
+  {
+    "revision": "a988d86a9b38cd5697be",
+    "url": "/MovieApp/static/js/2.c7f0978e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieApp/static/js/2.c7f0978e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0e4bf1bb43dacd7277",
+    "url": "/MovieApp/static/js/main.37629bef.chunk.js"
+  },
+  {
+    "revision": "899c9c9e1a5043059e91",
+    "url": "/MovieApp/static/js/runtime-main.8397dfc1.js"
+  }
+]);
